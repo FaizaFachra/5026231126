f1b55a9d42f631c8bbc5bb0e530e8525fc6bd363
Muhammad Faiza Fachra Azizi <fachrae@gmail.com>
2025-05-21 13:00:44 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Link;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,51 +18,60 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('pertemuan.frontend');
+    return view('frontend');
 });
 
 
 Route::get('/flex', function () {
-    return view('pertemuan.flex');
+    return view('flex');
 });
 
 Route::get('/index', function () {
-    return view('pertemuan.index');
+    return view('index');
 });
 
 Route::get('/indexdanantara', function () {
-    return view('pertemuan.indexdanantara');
+    return view('indexdanantara');
 });
 
 Route::get('/js1', function () {
-    return view('pertemuan.js1');
+    return view('js1');
 });
 
 Route::get('/js2', function () {
-    return view('pertemuan.js2');
+    return view('js2');
 });
 
 Route::get('/descendantselector', function () {
-    return view('pertemuan.descendantselector');
+    return view('descendantselector');
 });
 
 Route::get('/pertama', function () {
-    return view('pertemuan.pertama');
+    return view('pertama');
 });
 
 Route::get('/PERTEMUAN4', function () {
-    return view('pertemuan.PERTEMUAN4'); // jika nama file-nya "PERTEMUAN 4.html" dan sudah diubah jadi PERTEMUAN4.blade.php
+    return view('PERTEMUAN4'); // jika nama file-nya "PERTEMUAN 4.html" dan sudah diubah jadi PERTEMUAN4.blade.php
 });
 
 Route::get('/Sibling', function () {
-    return view('pertemuan.Sibling');
+    return view('Sibling');
 });
 
 Route::get('/TugasLinktree', function () {
-    return view('pertemuan.TugasLinktree'); 
+    return view('TugasLinktree'); 
 });
 
+Route::get('/dosen', [Link::class,'index']);
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
 
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class,'kontak']);
 
 
 
